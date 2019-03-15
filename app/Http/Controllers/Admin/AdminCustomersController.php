b53f4297e33bba468f85ee034c7db71ca5578885
lchtechhk <lchtechhhk@gmail.com>
2019-03-15 23:13:56 +0800

    1. To Do Customer

--- app/Http/Controllers/Admin/AdminCustomersController.php
+++ app/Http/Controllers/Admin/AdminCustomersController.php
@@ -43,8 +43,8 @@ class AdminCustomersController extends Controller
 		
 		$customers = DB::table('customers')
 			->LeftJoin('address_book','address_book.address_book_id','=', 'customers.customers_default_address_id')
-			->LeftJoin('countries','countries.countries_id','=', 'address_book.entry_country_id')
-			->LeftJoin('zones','zones.zone_id','=', 'address_book.entry_zone_id')
+			->LeftJoin('countries','countries.id','=', 'address_book.entry_country_id')
+			->LeftJoin('zones','zones.id','=', 'address_book.entry_zone_id')
 			->LeftJoin('customers_info','customers_info.customers_info_id','=', 'customers.customers_id')
 			->select('customers.*', 'address_book.entry_gender as entry_gender', 'address_book.entry_company as entry_company', 'address_book.entry_firstname as entry_firstname', 'address_book.entry_lastname as entry_lastname', 'address_book.entry_street_address as entry_street_address', 'address_book.entry_suburb as entry_suburb', 'address_book.entry_postcode as entry_postcode', 'address_book.entry_city as entry_city', 'address_book.entry_state as entry_state', 'countries.*', 'zones.*')
 			->orderBy('customers.customers_id','ASC')
